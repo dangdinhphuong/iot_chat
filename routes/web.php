52ce606d4e0416139763c3c5748632a1120aba53
dang dinh phuong <dangxuong2000@gmail.com>
2023-03-27 23:42:45 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\SocketController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::name('api')->prefix('api/')->group(function () {
     Route::get('/cate/With/Node', [ApiController::class, 'getAllWithNode'])->name('getAllWithNode');
@@ -25,3 +23,16 @@ Route::name('api')->prefix('api/')->group(function () {
     });
 });
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('dashboard');
+});
